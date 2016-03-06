0ed6fce322a90281284b60780265ac3b64bd0438
fergmj <mindy@mindyferguson.com>
2016-03-06 21:26:12 +0000

    Add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306204204) do
+ActiveRecord::Schema.define(version: 20160306212026) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160306204204) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
