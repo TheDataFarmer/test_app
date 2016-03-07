b5ae082e8befd61ff7f3d01052fae9828fafb855
fergmj <mindy@mindyferguson.com>
2016-03-07 02:23:22 +0000

    Authentication fonished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    redirect_to articles_path if logged_in?
+    # Change this to redirect to the dashboard is a user is logged in
   end
 
   def about
