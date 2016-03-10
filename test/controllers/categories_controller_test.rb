72ca78261f15b0936d04a6e66941c3f7c817b769
fergmj <mindy@mindyferguson.com>
2016-03-10 18:31:57 +0000

    Adding Views to the Categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,49 +1,30 @@
 require 'test_helper'
-
-class CategoriesControllerTest < ActionController::TestCase
-  def setup
-    @category = Category.create(name: "sports")
-  end
-
-  test "should get categories index" do
-    get :index
-    assert_response :success
-    #assert_not_nil assigns(:categories)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  #test "should create category" do
-  #  assert_difference('Article.count') do
-  #  post :create, article: { description: @article.description, title: @article.title }
-  #  end
-
-  #  assert_redirected_to article_path(assigns(:article))
-  #end
-
-  test "should show category" do
-    get :show, id: @category
-    assert_response :success
-  end
-
- # test "should get edit" do
- #   get :edit, id: @article
- #   assert_response :success
- # end
-
-  #test "should update category" do
-  #  patch :update, id: @article, article: { description: @article.description, title: @article.title }
-  #  assert_redirected_to article_path(assigns(:article))
-  #end
-
-  test "should destroy category" do
-    assert_difference('Category.count', -1) do
-      delete :destroy, id: @category
+  class CategoriesControllerTest < ActionController::TestCase
+    def setup
+      @category = Category.create(name: "sports")
+      @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
     end
 
+    test "should get categories index" do
+    get :index
+    assert_response :success
+    end
+
+    test "should get new" do
+      session[:user_id] = @user.id
+    get :new
+    assert_response :success
+    end
+
+    test "should get show" do
+    get(:show, {'id' => @category.id})
+    assert_response :success
+    end
+
+    test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
     assert_redirected_to categories_path
   end
 end
